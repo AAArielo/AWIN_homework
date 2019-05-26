e90d04c5b23af6f1c66ad45154c3c2b022feb0c9
AAArielo <51025194+AAArielo@users.noreply.github.com>
2019-05-26 11:54:20 +0800

    Add files via upload

---
+++ CreateTable.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]            NVARCHAR (128) NOT NULL,
+    [UserName]      NVARCHAR (MAX) NULL,
+    [PasswordHash]  NVARCHAR (MAX) NULL,
+    [SecurityStamp] NVARCHAR (MAX) NULL,
+    [Discriminator] NVARCHAR (128) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[AspNetRoles] (
+    [Id]   NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[AspNetUserRoles] (
+    [UserId] NVARCHAR (128) NOT NULL,
+    [RoleId] NVARCHAR (128) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUserRoles] PRIMARY KEY CLUSTERED ([UserId] ASC, [RoleId] ASC),
+    CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_RoleId]
+    ON [dbo].[AspNetUserRoles]([RoleId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [dbo].[AspNetUserRoles]([UserId] ASC);
+
+
+CREATE TABLE [dbo].[Estate]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UrbanArea] NVARCHAR(50) NULL, 
+    [Sign] NVARCHAR(50) NULL, 
+    [Region] NVARCHAR(MAX) NULL, 
+    [Area] FLOAT NULL, 
+    [Price] INT NULL, 
+    [UnitPrice] INT NULL
+);
